498296710fe0eaa6e5b9829e74500af6a03347b9
Ahmed Mostafa <sitesdesigner96@gmail.com>
2024-07-28 19:23:55 +0300

    modify name

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  pwa: {
+    name: 'My Awesome App', // Change this to your desired app name
+  },
 })
